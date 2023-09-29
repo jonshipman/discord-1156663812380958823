ddf503894f9a0a9b77db00c652135ffb8edb8e4e
Jon Shipman <jon@jonshipman.com>
2023-09-28 20:25:41 -0500

    init

---
+++ src/routes/(maps)/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { PUBLIC_GMAP } from '$env/static/public';
+</script>
+
+<iframe
+	class="w-100 bn"
+	height="550"
+	frameborder="0"
+	referrerpolicy="no-referrer-when-downgrade"
+	src="https://www.google.com/maps/embed/v1/view?key={PUBLIC_GMAP}&center=46.414382,10.013988"
+	allowfullscreen
+	title="googlemaps"
+/>
+
+<div><slot /></div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<div class="pa4">
+	<div class="mw7 br4 pa4 bg-washed-green">
+		<slot />
+
+		<p><a href="/">Home</a></p>
+		<p><a href="/1">Map # 1</a></p>
+		<p><a href="/2">Map # 2</a></p>
+		<p><a href="/3">Map # 3</a></p>
+	</div>
+</div>
